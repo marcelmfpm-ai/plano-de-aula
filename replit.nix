72127260f44ef6da460f5dd11488dd38aa530028
marcelmfpm <marcel.mfpm@gmail.com>
2026-05-05 15:17:13 -0300

    fix: replit.nix com libstdc++ e auto-install de dependencias

--- replit.nix
+++ replit.nix
@@ -2,5 +2,14 @@
   deps = [
     pkgs.python3
     pkgs.python3Packages.pip
+    pkgs.stdenv.cc.cc.lib
+    pkgs.zlib
+    pkgs.gcc
   ];
+  env = {
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+    ];
+  };
 }
